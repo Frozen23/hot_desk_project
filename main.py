a6c8918a93a884fcd0376318c004068ea23a5e7b
Negariom <rafbadura@gmail.com>
2026-06-16 20:36:48 +0200

    poolfix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routers import company
 from routers import desks
 from routers import reservation
 
+create_database_schema()
 
 app = FastAPI(title="Hot Desk API") 
 
@@ -13,4 +14,4 @@ app.include_router(reservation.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
